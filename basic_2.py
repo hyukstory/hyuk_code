f4a7fa78f3a5be2c0569449f749f6c97d47fe39b
hyukstory <dlgurtncjswo@gmail.com>
2021-01-28 01:33:33 +0900

    python(1) 기초 연습

---
+++ basic_2.py
@@ -0,0 +1,25 @@
+# 숫자 up & down 게임 만들기
+# - while 과 if 문 활용
+import random
+
+rn = random.randrange(1, 101, 1)
+user = 0
+count = 0
+
+print("1~100 숫자 up % down 게임을 시작합니다.")
+
+while user != rn:
+
+    user = int(input("1 ~ 100 사이의 숫자를 입력하세요 : "))
+
+    if user > rn:
+        print("down")
+
+    elif user < rn:
+        print("up")
+
+    count += 1
+
+print("정답입니다.")
+
+print(count, "번만에 성공하셨습니다.")
